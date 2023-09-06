192d7372c65ef9e74bacc4ac582e78e3667eac3f
Loresea753 <88130019+Loresea753@users.noreply.github.com>
2023-09-07 01:20:13 +0700

    Unique Create and Update checker

--- src/main/java/com/example/intat3/repositories/UserRepository.java
+++ src/main/java/com/example/intat3/repositories/UserRepository.java
@@ -1,6 +1,8 @@
 package com.example.intat3.repositories;
 
 import com.example.intat3.Entity.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,4 +16,11 @@ public interface UserRepository extends CustomUserRepository<User, Integer> {
     boolean existsByUsername(String username);
     boolean existsByName(String name);
     boolean existsByEmail(String email);
+
+    @Query("SELECT CASE WHEN COUNT(u) > 1 THEN true ELSE false END FROM User u WHERE u.id = :userId OR u.username = :username")
+    boolean doesUserExistByUsername(@Param("userId") Integer userId, @Param("username") String username);
+    @Query("SELECT CASE WHEN COUNT(u) > 1 THEN true ELSE false END FROM User u WHERE u.id = :userId OR u.name = :name")
+    boolean doesUserExistByName(@Param("userId") Integer userId, @Param("name") String name);
+    @Query("SELECT CASE WHEN COUNT(u) > 1 THEN true ELSE false END FROM User u WHERE u.id = :userId OR u.email = :email")
+    boolean doesUserExistByEmail(@Param("userId") Integer userId, @Param("email") String email);
 }
--- src/main/java/com/example/intat3/validation/UniqueValidator.java
+++ src/main/java/com/example/intat3/validation/UniqueValidator.java
@@ -1,8 +1,13 @@
 package com.example.intat3.validation;
 
 import com.example.intat3.repositories.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
 
@@ -11,6 +16,8 @@ public class UniqueValidator implements ConstraintValidator<UniqueValid,String> 
     public UserRepository repository1;
     boolean nameCheck;
     boolean usernameCheck;
+
+//    private Integer id ;
 
     public UniqueValidator(UserRepository repository1) {
         this.repository1 = repository1;
@@ -24,11 +31,25 @@ public class UniqueValidator implements ConstraintValidator<UniqueValid,String> 
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext){
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        String idPath = request.getRequestURI().split("/").length <= 3?null: request.getRequestURI().split("/")[3];
         if(value == null){
             return true;
         }
-        boolean valueValid = this.usernameCheck?this.repository1.existsByUsername(value):
-                this.nameCheck?this.repository1.existsByName(value):this.repository1.existsByEmail(value);
-        return !valueValid;
+        if(idPath == null){
+            boolean valueValid = this.usernameCheck?this.repository1.existsByUsername(value):
+                    this.nameCheck?this.repository1.existsByName(value):this.repository1.existsByEmail(value);
+            return !valueValid;
+        }else {
+            Integer id = Integer.valueOf(idPath);
+            boolean updateValid = this.usernameCheck?this.repository1.doesUserExistByUsername(id,value):
+                    this.nameCheck?this.repository1.doesUserExistByName(id,value):this.repository1.doesUserExistByEmail(id,value);
+            return !updateValid;
+        }
+
+
     }
+
+
 }
